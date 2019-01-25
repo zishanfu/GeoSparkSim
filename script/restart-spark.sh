d30ccb1b98dfd3e44334daf55191ccdb0b939c1b
ZishanFu <fuzishan0816@gmail.com>
2019-01-25 16:24:13 -0700

    update

---
+++ script/restart-spark.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+/hdd2/code/spark-2.3.2-bin-hadoop2.6/sbin/stop-all.sh
+/hdd2/code/spark-2.3.2-bin-hadoop2.6/sbin/start-all.sh
--- script/test1.sh
+++ script/test1.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-sparkcommand="../../spark-2.3.2-bin-hadoop2.6/bin/spark-submit --master spark://en4119507l.cidse.dhcp.asu.edu:7077 --class com.zishanfu.vistrips.App /hdd2/code/zishanfu/GeoSparkSim/target/VisTrips_v1-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+sparkcommand="../../../spark-2.3.2-bin-hadoop2.6/bin/spark-submit --master spark://en4119507l.cidse.dhcp.asu.edu:7077 --class com.zishanfu.vistrips.App /hdd2/code/zishanfu/GeoSparkSim/target/VisTrips_v1-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 hdfs_path="hdfs://en4119507l.cidse.dhcp.asu.edu:54310"
 osm_path="/hdd2/code/zishanfu/GeoSparkSim/src/test/resources/map.osm"
 sleepinterval=60
@@ -10,4 +10,6 @@ simulation=10
 partition=5
 
 $sparkcommand $number $timestamp $simulation $partition $hdfs_path osm_path
-sleep $sleepinterval
+sleep $sleepinte
+
+./restart-spark.sh
