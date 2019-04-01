9274422f05af5310a16122c12ebdb3cecff206d5
ZishanFu <fuzishan0816@gmail.com>
2019-03-31 18:07:58 -0700

    update experiments main class

--- script/dummy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sparkcommand="../../../../spark-2.3.2-bin-hadoop2.6/bin/spark-submit --master spark://en4119507l.cidse.dhcp.asu.edu:7077 --class com.zishanfu.geosparksim.GeoSparkSim /hdd2/code/zishanfu/GeoSparkSim/GeoSparkSim/target/GeoSparkSim-1.0-SNAPSHOT-jar-with-dependencies.jar"
-checksize="../../../../hadoop-2.6.5/bin/hadoop fs -du -s -h /vistrips/reports_*"
-
-$sparkcommand -n 100000 -p 500 -r 120
-sleep $sleepinterval
-
--- script/timeTest.sh
+++ script/timeTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sparkcommand="../../../spark-2.3.2-bin-hadoop2.6/bin/spark-submit --master spark://en4119507l.cidse.dhcp.asu.edu:7077 --class GeoSparkSim /hdd2/code/zishanfu/GeoSparkSim/GeoSparkSim/target/GeoSparkSim-1.0-SNAPSHOT-jar-with-dependencies.jar"
-checksize="../../../hadoop-2.6.5/bin/hadoop fs -du -s -h /vistrips/reports*"
+sparkcommand="../../../spark-2.3.2-bin-hadoop2.6/bin/spark-submit --master spark://en4119507l.cidse.dhcp.asu.edu:7077 --class com.zishanfu.geosparksim.GeoSparkSim /hdd2/code/zishanfu/GeoSparkSim/GeoSparkSim/target/GeoSparkSim-1.0-SNAPSHOT-jar-with-dependencies.jar"
+checksize="../../../hadoop-2.6.5/bin/hadoop fs -du -s -h /geosparksim/reports*"
 
 hdfs_path="hdfs://en4119507l.cidse.dhcp.asu.edu:54310"
 
